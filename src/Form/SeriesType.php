d0d72c11be1d1a06280be83a04a254c0c7f5e362
Pierre <pierre.nogaro@gmail.com>
2024-10-15 11:48:01 +0200

    CRUD series + templates & importmap bootstrap

---
+++ src/Form/SeriesType.php
@@ -0,0 +1,49 @@
+<?php
+
+// src/Form/SeriesType.php
+
+namespace App\Form;
+
+use App\Entity\Series;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeriesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('publish_date', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de publication',
+            ])
+            ->add('critical_rate', IntegerType::class, [
+                'required' => false,
+                'label' => 'Note critique',
+            ])
+            ->add('seen', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Vu',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Series::class,
+        ]);
+    }
+}
+
