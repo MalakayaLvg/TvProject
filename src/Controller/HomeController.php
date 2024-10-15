fddf6b4985feb32b85873a477e8de2bcdddeed35
raphael <raphael.moynat@gmail.com>
2024-10-15 15:42:15 +0200

    user raph

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(RequestService $service): Response
     {
-        $series = $service->getSeries();
-        dd($series);
+
+
         return $this->render('home/index.html.twig',);
     }
 }
