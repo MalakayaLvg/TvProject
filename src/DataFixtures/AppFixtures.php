d955fb7192a5b52ccf0c966e1a860334a12c3b57
Mey Schibboleths <meynever@gmail.com>
2024-10-15 11:38:36 +0000

    Update AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,8 +11,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-         $film = new Film();
-         $manager->persist($film);
+       
 
         $manager->flush();
     }
